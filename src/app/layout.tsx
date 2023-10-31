fcbe1e3124f5263492b5046a215b81daab46b626
Macwel <wifi-215@yandex.ru>
2023-10-31 15:06:58 +0300

    add ya metrics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from "next";
+import Script from "next/script";
 
 
 export const metadata: Metadata = {
@@ -18,6 +19,30 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <Script id="yandex-metrica" type="text/javascript">
+          {`
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(94753079, "init", {
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true
+    });
+   `}
+        </Script>
+        <noscript>
+          <img
+            id={"yandex-metrica123"}
+            src={"https://mc.yandex.ru/watch/94753079"}
+            style={{ position: "absolute", left: "-9999px" }}
+            alt={""}
+          />
+        </noscript>
       </body>
     </html>
   );
