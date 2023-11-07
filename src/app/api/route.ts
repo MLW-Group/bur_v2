a7a2dbbb3cf838f7a230f0aea0d5e4547ae95afa
Sandr Klimenko <45948961+Macwel@users.noreply.github.com>
2023-11-07 12:02:25 +0300

    add await

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,10 +2,15 @@
 import { Telegraf } from "telegraf";
 
 export async function POST(request: Request) {
+  try {
     const body = await request.json()
-  const bot = new Telegraf("6010983445:AAHu5iPqEMRx-7JAp7SvYMkd0bVNm_mpI5k");
+    const bot = new Telegraf("6010983445:AAHu5iPqEMRx-7JAp7SvYMkd0bVNm_mpI5k");
+  
+    await bot.telegram.sendMessage(977981822, body?.phone);
+  } catch (error) {
+    console.log(error)
+  }
 
-  bot.telegram.sendMessage(977981822, body?.phone);
 
   return Response.json({});
 }
