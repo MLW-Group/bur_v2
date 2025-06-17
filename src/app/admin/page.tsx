089a852583e14082a360825af2807cad9b5b8495
Qoupha <range86775@gmail.com>
2025-06-17 17:11:08 +0300

    add ssr,auth in cookie,delete localStorage

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,60 +1,41 @@
-"use client";
-import Main from "@/components/Admin/Main";
-import Sidebar from "@/components/Admin/Sidebar";
-import { Header } from "@/components/Header";
-import "../global.css";
-import SignInForm from "@/components/SignInForm";
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-import { AppContext } from "@/context/app-context";
-import { useRouter } from "next/navigation";
-import { useCookies } from "next-client-cookies";
-import axios from "axios";
+import '../global.css';
+import SignInForm from '@/components/SignInForm';
+import axios from 'axios';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-const Block = styled.div`
-  // width: 100vw;
-  background: linear-gradient(
-      0deg,
-      rgba(255, 255, 255, 0.05) 0%,
-      rgba(255, 255, 255, 0.05) 100%
-    ),
-    url(/img/grid.png), black 50% / cover no-repeat;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-export default function AdminPage() {
-  const router = useRouter();
-  const getCurrentUser = async (token: string) => {
-    try {
-      const { data } = await axios.get(
-        `https://bur-api.macwel.app/api/v1/user/me`,
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
-      if (data) {
-        router.push("/admin/order");
-      }
-    } catch (error) {
-      console.log("🚀 ~ getCurrentUser ~ error:", error);
-    }
-  };
-  useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken")! as string;
-    getCurrentUser(accessToken);
-  }, []);
-  return (
-    <Block>
-      <div className={"container"} id="container">
-        <SignInForm />
-        <div className="overlay-container">
-          <div className="overlay">
-            <div className="overlay-panel overlay-right"></div>
-          </div>
-        </div>
-      </div>
-    </Block>
-  );
+export default async function AdminPage() {
+	const cookieStore = await cookies();
+	const token = cookieStore.get('accessToken')?.value;
+	console.log('🚀 ~ AdminPage ~ token:', token);
+	if (token) {
+		const { data } = await axios.get(`https://bur-api.macwel.app/api/v1/user/me`, {
+			headers: { Authorization: `Bearer ${token}` },
+		});
+		if (data?.data) {
+			redirect('/admin/order');
+		}
+	}
+
+	return (
+		<div
+			style={{
+				height: '100vh',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				background:
+					'linear-gradient(0deg, rgba(255, 255, 255, 0.05) 0%, rgba(255, 255, 255, 0.05) 100%), url(/img/grid.png), black 50% / cover no-repeat;',
+			}}
+		>
+			<div className={'container'} id="container">
+				<SignInForm />
+				<div className="overlay-container">
+					<div className="overlay">
+						<div className="overlay-panel overlay-right"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/app/admin/panel/page.tsx
+++ src/app/admin/panel/page.tsx
@@ -1,48 +1,22 @@
-"use client";
-import Main from "@/components/Admin/Main";
-import Sidebar from "@/components/Admin/Sidebar";
-import { Header } from "@/components/Header";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import styled from "styled-components";
-const Block = styled.div`
-  // width: 100vw;
-  background: linear-gradient(
-      0deg,
-      rgba(255, 255, 255, 0.05) 0%,
-      rgba(255, 255, 255, 0.05) 100%
-    ),
-    url(/img/grid.png), black 50% / cover no-repeat;
-  min-height: 100vh;
-`;
-export default function AdminPage() {
-  const router = useRouter();
-  const getCurrentUser = async (token: string) => {
-    try {
-      const { data } = await axios.get(
-        `https://bur-api.macwel.app/api/v1/marker`,
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
-      // if (data) {
-      //   router.push("/admin/order");
-      // }
-    } catch (error) {
-      if (error) {
-        router.push("/admin");
-      }
-    }
-  };
-  useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken")! as string;
-    getCurrentUser(accessToken);
-  }, []);
-  return (
-    <Block>
-      <Sidebar />
-      <Main />
-    </Block>
-  );
+import Main from '@/components/Admin/Main';
+import { cookies } from 'next/headers';
+import Sidebar from '@/components/Admin/Sidebar';
+import axios from 'axios';
+import { redirect } from 'next/navigation';
+import LayoutAdmin from '@/components/LayoutAdmin';
+
+export default async function AdminPage() {
+	const cookieStore = await cookies();
+	const token = cookieStore.get('accessToken')?.value;
+	if (!token) redirect('/admin');
+
+	const getMarks = await axios.get(`https://bur-api.macwel.app/api/v1/marker`, {
+		headers: { Authorization: `Bearer ${token}` },
+	});
+	return (
+		<LayoutAdmin>
+			<Sidebar />
+			<Main marksData={getMarks.data} />
+		</LayoutAdmin>
+	);
 }
