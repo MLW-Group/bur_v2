49230d7303c8a95d953415c1620f9d18967d45a5
Macwel <wifi-215@yandex.ru>
2025-07-22 12:55:05 +0300

    add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,55 +1,63 @@
-import GlobalStyle from "@/styles/globalstyles";
-import Script from "next/script";
-import { Metadata } from "next";
-import Providers from "@/Providers/Providers";
+import GlobalStyle from '@/styles/globalstyles';
+import Script from 'next/script';
+import { Metadata } from 'next';
+import Providers from '@/Providers/Providers';
 
 export const metadata: Metadata = {
-  title: "Бурение скважин на воду в Челябинской области",
-  description: "Бурение скважин на воду",
+	title: 'Бурение скважин на воду в Челябинской области',
+	description: 'Бурение скважин на воду',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          charSet="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        />
-      </head>
-      <body
-        style={{
-          overflowY: "scroll",
-          scrollBehavior: "smooth",
-        }}
-      >
-        <Providers>{children}</Providers>
-        {process.env.NODE_ENV !== "development" && (
-          <Script id="yandex-metrica" type="text/javascript">
-            {`
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html>
+			<head>
+				{process.env.NODE_ENV !== 'development' && (
+					<Script id="yandex-metrica" type="text/javascript">
+						{`
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PV8HXG22');</script>
+<!-- End Google Tag Manager -->
+        `}
+					</Script>
+				)}
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+				<link
+					href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300&display=swap"
+					rel="stylesheet"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
+					rel="stylesheet"
+				/>
+				<link
+					rel="stylesheet"
+					type="text/css"
+					charSet="UTF-8"
+					href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+				/>
+				<link
+					rel="stylesheet"
+					type="text/css"
+					href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+				/>
+			</head>
+			<body
+				style={{
+					overflowY: 'scroll',
+					scrollBehavior: 'smooth',
+				}}
+			>
+				<Providers>{children}</Providers>
+				{process.env.NODE_ENV !== 'development' && (
+					<Script id="yandex-metrica" type="text/javascript">
+						{`
     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
     m[i].l=1*new Date();
     for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
@@ -63,17 +71,25 @@ export default function RootLayout({
           webvisor:true
     });
    `}
-          </Script>
-        )}
-        <noscript>
-          <img
-            id={"yandex-metrica123"}
-            src={"https://mc.yandex.ru/watch/94753079"}
-            style={{ position: "absolute", left: "-9999px" }}
-            alt={""}
-          />
-        </noscript>
-      </body>
-    </html>
-  );
+					</Script>
+				)}
+				<noscript>
+					<img
+						id={'yandex-metrica123'}
+						src={'https://mc.yandex.ru/watch/94753079'}
+						style={{ position: 'absolute', left: '-9999px' }}
+						alt={''}
+					/>
+				</noscript>
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-PV8HXG22"
+						height="0"
+						width="0"
+						style={{ display: 'none', visibility: 'hidden' }}
+					></iframe>
+				</noscript>
+			</body>
+		</html>
+	);
 }
